e4c5b3e0e255a8f20022f3c56a5c9e6bfb661c92
KropaPL <k0n42525@gmail.com>
2024-08-12 15:25:27 +0200

    Main View Changes

--- BeekeeperApp/BeekeeperApp/Controllers/HomeController.cs
+++ BeekeeperApp/BeekeeperApp/Controllers/HomeController.cs
@@ -2,8 +2,11 @@
 
 namespace BeekeeperApp.Controllers
 {
+    [Route("[controller]")]
     public class HomeController : Controller
     {
+        [Route("/[action]")]
+        [Route("/")]
         public async Task<IActionResult> Index()
         {
             return View();
